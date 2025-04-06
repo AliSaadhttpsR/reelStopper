5e75dbead2d3e642f083a6145d1187cf361be11d
AliSaadhttpsR <88747988+AliSaadhttpsR@users.noreply.github.com>
2025-04-06 20:06:37 +0300

    the code

---
+++ background_script.js
@@ -0,0 +1,40 @@
+
+
+  function myFunction(){
+    alert("stop wasting your time");
+    window.open('https://www.youtube.com','_self');
+    }
+    
+
+
+
+document.addEventListener("click", myFunction)
+
+
+
+/* ;   */
+
+
+
+
+      /* {
+
+
+    "manifest_version": 3,
+    "name": "Firefox tabs to Insta",
+    "version": "0.0.1",
+    "icons": {
+      "64": "icons/twitch.ico"
+    },
+
+    "content_scripts": [
+      {
+          "matches":["https://www.instagram.com/explore/"],
+          "js":["background_script.js"]
+      }
+  ]
+
+
+
+  } */
+        
